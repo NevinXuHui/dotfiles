8d38b2d530d0de2958ee1bc347c534e8e70686c2
xuhui <xuhui@cmhi.chinamobile.com>
2023-04-25 17:31:25 +0800

    配置初始化

---
+++ dot_auto_start.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+# description: do something while X-window starts
+# file: ~/.auto_start.sh
+# author: StableGenius
+# mail: feng@dongfa.pro
+# date: 2021-09-02
+
+# change wallpaper periodically
+# bash ~/.scripts/wallpaper/change_wallpaper_periodically.sh &
+
+# power manager
+# mate-power-manager &
+
+# download a new wallpaper from bing.com
+# python ~/.scripts/wallpaper/bing_today_wallpaper_crawler.py &
+
+# input method
+fcitx5 -d &
+
+# alpha terminal 
+# sudo pacman -S picom
+picom &
+
+utools &
+
+# proxy
+# qv2ray &
+
+# status bar 
+slstatus &
